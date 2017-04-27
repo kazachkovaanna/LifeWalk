5f1d14ef85e4e687de30b49570a1480a450032ba
kazachkovaanna <kazachkova.ann@gmail.com>
2017-04-28 02:09:14 +0300

    Добавление флажка на карту

--- app/src/main/java/spblife/walk/NearbyActivity.java
+++ app/src/main/java/spblife/walk/NearbyActivity.java
@@ -1,6 +1,7 @@
 package spblife.walk;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,12 +12,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import ru.yandex.yandexmapkit.MapController;
+import ru.yandex.yandexmapkit.MapView;
+import ru.yandex.yandexmapkit.OverlayManager;
+import ru.yandex.yandexmapkit.overlay.Overlay;
+import ru.yandex.yandexmapkit.overlay.OverlayItem;
+import ru.yandex.yandexmapkit.overlay.balloon.BalloonItem;
+import ru.yandex.yandexmapkit.utils.GeoPoint;
+import spblife.walk.nearby.BalloonListener;
 import spblife.walk.settings.Settings;
 
 public class NearbyActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     Settings settings;
+    MapController mapController;
+    OverlayManager overlayManager;
+    BalloonListener balloonListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +46,17 @@ public class NearbyActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //Настройки
         settings = new Settings(getSharedPreferences(".settings", Context.MODE_PRIVATE));
+
+        //Работа с картой
+        MapView mapView = (MapView) findViewById(R.id.map);
+        mapController = mapView.getMapController();
+        overlayManager = mapController.getOverlayManager();
+        overlayManager.getMyLocation().setEnabled(true);
+        balloonListener = new BalloonListener();
+        //ну и поработать с ней
+        go();
     }
 
     @Override
@@ -74,5 +96,32 @@ public class NearbyActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private  void go(){
+        //Создать  слой для работы с объектами
+        Resources res = getResources();
+        Overlay overlay = new Overlay(mapController);
+        OverlayItem overlayItem = new OverlayItem(new GeoPoint(59.987313, 30.370817),res.getDrawable(R.drawable.place, getTheme()));
+        BalloonItem balloonItem = new BalloonItem(this,overlayItem.getGeoPoint());
+        balloonItem.setOnBalloonListener(balloonListener);
+        overlay.addOverlayItem(overlayItem);
+        overlayManager.addOverlay(overlay);
+        //Перемещение карты из Москвы в Положение точки на карте
+        mapController.setPositionAnimationTo(overlayItem.getGeoPoint());
+        //Масштабирование
+        /*double maxLat, minLat, maxLon, minLon;
+        maxLat = maxLon = Double.MIN_VALUE;
+        minLat = minLon = Double.MAX_VALUE;
+        GeoPoint geoPoint = overlayItem.getGeoPoint();
+        double lat = geoPoint.getLat();
+        double lon = geoPoint.getLon();
+
+        maxLat = Math.max(lat, maxLat);
+        minLat = Math.min(lat, minLat);
+        maxLon = Math.max(lon, maxLon);
+        minLon = Math.min(lon, minLon);
+
+        mapController.setZoomToSpan(-10, -10);*/
     }
 }
---
+++ app/src/main/java/spblife/walk/nearby/BalloonListener.java
@@ -0,0 +1,37 @@
+package spblife.walk.nearby;
+
+import android.view.View;
+
+import ru.yandex.yandexmapkit.overlay.balloon.BalloonItem;
+import ru.yandex.yandexmapkit.overlay.balloon.OnBalloonListener;
+
+/**
+ * Created by kazac on 28.04.2017.
+ */
+public class BalloonListener implements OnBalloonListener {
+
+    @Override
+    public void onBalloonViewClick(BalloonItem balloonItem, View view) {
+
+    }
+
+    @Override
+    public void onBalloonShow(BalloonItem balloonItem) {
+
+    }
+
+    @Override
+    public void onBalloonHide(BalloonItem balloonItem) {
+
+    }
+
+    @Override
+    public void onBalloonAnimationStart(BalloonItem balloonItem) {
+
+    }
+
+    @Override
+    public void onBalloonAnimationEnd(BalloonItem balloonItem) {
+
+    }
+}
